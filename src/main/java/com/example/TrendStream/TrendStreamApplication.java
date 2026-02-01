54eac46ec903e8a37593bb871b6e265535802a4a
MelonSeo <biniuniv@naver.com>
2026-02-01 16:54:30 +0900

    git init
    first init

---
+++ src/main/java/com/example/TrendStream/TrendStreamApplication.java
@@ -0,0 +1,13 @@
+package com.example.TrendStream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrendStreamApplication.class, args);
+	}
+
+}
